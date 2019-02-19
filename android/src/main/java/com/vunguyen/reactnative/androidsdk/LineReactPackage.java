367de048624eb43cabd7ac1849c5f5942d1fc96e
test <test@test.com>
2019-02-19 13:36:03 +0800

    update

--- android/src/main/java/com/vunguyen/reactnative/androidsdk/LineReactPackage.java
+++ android/src/main/java/com/vunguyen/reactnative/androidsdk/LineReactPackage.java
@@ -15,10 +15,10 @@ import java.util.List;
  */
 public class LineReactPackage implements ReactPackage {
 
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
+//    @Override
+//    public List<Class<? extends JavaScriptModule>> createJSModules() {
+//        return Collections.emptyList();
+//    }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
